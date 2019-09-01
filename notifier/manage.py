45db54544970344e026a6183c70757fa3891643f
bertini36 <albertopouquiros@gmail.com>
2019-09-01 23:20:48 +0200

    Workers

--- notifier/manage.py
+++ notifier/manage.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
 
 from app import NotifierApp
-from app.settings import HOST, PORT, DEBUG
+from app.settings import HOST, PORT, DEBUG, WORKERS
 
 
 if __name__ == '__main__':
     app = NotifierApp()
-    app.run(host=HOST, port=PORT, debug=DEBUG)
+    app.run(host=HOST, port=PORT, debug=DEBUG, workers=WORKERS)
 
